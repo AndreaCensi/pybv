81ae3a78f309b28c8c2ed5ba6ed0f0319ad766c9
Andrea Censi <andrea@cds.caltech.edu>
2010-04-28 19:16:26 +0000

    removed old files

--- simulation/utils.py
+++
@@ -1,7 +0,0 @@
-import progressbar
-
-def create_progress_bar(job_id, total_iterations):
-    widgets = [job_id, ' ', progressbar.Percentage(), ' ', progressbar.Bar(marker=progressbar.RotatingMarker()), ' ', progressbar.ETA()]
-    pbar = progressbar.ProgressBar(widgets=widgets, maxval=total_iterations).start()
-    return pbar
-    
